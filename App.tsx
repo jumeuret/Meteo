3ff36ca54dcbb0e7a3bc96d872087ee5aa5a5749
Laurine <laurine.brette@gmail.com>
2023-02-05 15:20:35 +0100

    creation d'un component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Float } from 'react-native/Libraries/Types/CodegenTypes';
+import DayWheather from "./Component/DayWeather";
+import React from 'react'
 
-export default function App() {
+export default function App(){
   return (
     <View style={styles.container}>
-      <Text>Hello, I'm your app ! ^_^</Text>
+      <Text>ss</Text>
       <Text>And I'm really</Text>
       <View style={styles.beauty}>
         <Text>BEAUTIFUL</Text>
         </View>
       <StatusBar style="auto" />
+      <DayWheather/>
     </View>
+
   );
 }
 
